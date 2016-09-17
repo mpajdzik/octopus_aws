b6ff5a1625378634299e078895a3c5343309793b
Marcin Pajdzik <marcin.pajdzik@codewriters.co.uk>
2016-09-18 00:26:48 +0100

    Update octopus_deregister_lambda.js

--- octopus_deregister_lambda.js
+++ octopus_deregister_lambda.js
@@ -1,17 +1,12 @@
 var aws = require("aws-sdk");
 exports.handler = function(event, context) {
-      if (event.detail.state != "terminated")
-      context.succeed(event);
-    
+  if (event.detail.state != "terminated")
+    context.succeed(event);
     var http = require('http');
-  
-    var instanceId = event.detail["instance-id"]; // [""] required because of the hyphen
+    var instanceId = event.detail["instance-id"];
     var currentRegion = event.region;
-
     console.log('EC2InstanceId =', instanceId);
-
-    var ec2 = new aws.EC2({region: currentRegion}); //event.ResourceProperties.Region});
-
+    var ec2 = new aws.EC2({region: currentRegion});
     var params = {
         DryRun: false,
         Filters: [
@@ -28,51 +23,41 @@ exports.handler = function(event, context) {
             ]
           },
         ],
-        MaxResults: 5,
+      MaxResults: 5,
     };
     console.log('Filters =', params);
     console.log("Getting MachineName for InstanceID: " + instanceId);
-    
     ec2.describeTags(params, function(err, data) {
-
-        if (err) 
-        {   
-
-            console.log(err, err.stack); // an error occurred
-            context.succeed(err);
-        }
-        else 
-        {
-
-            console.log(data);           // successful response
-            var octopusMachineId = data.Tags[0].Value;
-            var fullPath = '/api/machines/' +  octopusMachineId + '?apiKey=<Octopus API Key>'; // API-XXXXXXXXXXXXXXXXXXXXXXXXXX
-    
-            var options = {
-              host: '<Octopus Server Private IP>',
-              port: 80,
-              path: fullPath,
-              method: 'Delete'
-            };
-            console.log("At parmeter section");
-                
-              callback = function(response) {
-              var str = '';
-              response.on('data', function (chunk) {
-              str += chunk;
-              });
-            
-              response.on('end', function () {
-                console.log(str);
-                context.succeed(str);
-              });
-            console.log("Making HTTP request with");  
-            
-            };
-            http.request(options, callback).end();
-            
-        }
-            
-        }
-    );
+    if (err) 
+      {   
+        console.log(err, err.stack); // an error occurred
+        context.succeed(err);
+      }
+    else 
+      {
+        console.log(data);           // successful response
+        var octopusMachineId = data.Tags[0].Value;
+        var fullPath = '/api/machines/' +  octopusMachineId + '?apiKey=<Octopus API Key>'; // API-XXXXXXXXXXXXXXXXXXXXXXXXXX
+        var options = {
+          host: '<Octopus Server Private IP>',
+          port: 80,
+          path: fullPath,
+          method: 'Delete'
+        };
+        console.log("At parmeter section");
+        callback = function(response) {
+          var str = '';
+          response.on('data', function (chunk) {
+            str += chunk;
+          });
+          response.on('end', function () {
+            console.log(str);
+            context.succeed(str);
+          });
+          console.log("Making HTTP request with");  
+        };
+        http.request(options, callback).end();
+      }
+    }
+  );
 };
